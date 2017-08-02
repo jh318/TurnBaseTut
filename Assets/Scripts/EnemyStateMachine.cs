f1bb88c924f7395bc5442c4d06cf9b3aee1df29d
John Hawley <jhawley@institute.local>
2017-08-01 23:24:06 -0500

    init commit. completed part 02

---
+++ Assets/Scripts/EnemyStateMachine.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStateMachine : MonoBehaviour {
+
+	public BaseEnemy enemy;
+
+	
+}
---
+++ Assets/Scripts/HeroStateMachine.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroStateMachine : MonoBehaviour {
+
+	public BaseHero hero;
+	
+}
