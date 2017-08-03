59a5821568972299fd0d199a1a8b0a7d36ec4186
John Hawley <jhawley@institute.local>
2017-08-03 00:01:23 -0500

    on part 05, 18:06

---
+++ Assets/Scripts/BattleStateMachine.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class BattleStateMachine : MonoBehaviour {
+
+	public enum PerformAction{
+		WAIT,
+		TAKEACTION,
+		PERFORMACTION
+	}
+
+	public PerformAction battleStates;
+
+	public List<HandleTurn> PerformList = new List<HandleTurn>();
+
+	public List<GameObject> HerosInGame = new List<GameObject>();
+	public List<GameObject> EnemysInBattle = new List<GameObject>();
+
+
+	void Start () {
+		battleStates = PerformAction.WAIT;
+		EnemysInBattle.AddRange(GameObject.FindGameObjectsWithTag("Enemy"));
+	}
+	
+	void Update () {
+		switch(battleStates){
+			case(PerformAction.WAIT):
+				break;
+			case(PerformAction.TAKEACTION):
+				break;
+			case(PerformAction.PERFORMACTION):
+				break;
+		}
+	}
+}
---
+++ Assets/Scripts/HandleTurn.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleTurn {
+
+	public string Attacker;
+	public GameObject AttacksGameObject;
+	public GameObject AttackersTarget;
+
+	public 
+}
