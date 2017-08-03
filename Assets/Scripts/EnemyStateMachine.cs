4d5e49ad133f454f9a7af3278c81d96ef767e958
John Hawley <jhawley@institute.local>
2017-08-02 22:53:50 -0500

    completed part 04

--- Assets/Scripts/EnemyStateMachine.cs
+++ Assets/Scripts/EnemyStateMachine.cs
@@ -6,5 +6,28 @@ public class EnemyStateMachine : MonoBehaviour {
 
 	public BaseEnemy enemy;
 
-	
+	public enum TurnState{
+		PROCESSING,
+		ADDTOLIST,
+		WAITING,
+		SELECTING,
+		ACTION,
+		DEAD
+	}
+
+	public TurnState currentState;
+	//for progress bar
+	private float cur_cooldown = 0.0f;
+	private float max_cooldown = 5.0f;
+
+	void Start(){
+		currentState = TurnState.PROCESSING;
+	}
+
+	void UpgradeProgressBar(){
+		cur_cooldown = cur_cooldown + Time.deltaTime;
+		if(cur_cooldown >= max_cooldown){
+			currentState = TurnState.ADDTOLIST;
+		}
+	}
 }
